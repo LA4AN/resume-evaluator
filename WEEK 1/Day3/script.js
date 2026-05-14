99c1050bdbfaf3e15166c9d91d176c81db00e3ec
Layan <layanalmanaa@gmail.com>
2026-05-14 15:55:05 +0300

    cleanup: remove unnecessary folders and keep resume-evaluator

--- WEEK 1/Day3/script.js
+++
@@ -1,8 +0,0 @@
-const nwDiv = document.createElement("div");
-nwDiv.textContent = "hello world";
-nwDiv.classList.add('my-class');
-
-const container = document.getElementById("container")
-container.appendChild(nwDiv);
-
-container.addEventListener()
