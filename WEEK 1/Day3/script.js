b2a69cdd182bead324ba28a526afcc543b17e7f7
Layan <layanalmanaa@gmail.com>
2026-05-14 16:03:38 +0300

    fix: restore folder structure and upload resume files

---
+++ WEEK 1/Day3/script.js
@@ -0,0 +1,8 @@
+const nwDiv = document.createElement("div");
+nwDiv.textContent = "hello world";
+nwDiv.classList.add('my-class');
+
+const container = document.getElementById("container")
+container.appendChild(nwDiv);
+
+container.addEventListener()
